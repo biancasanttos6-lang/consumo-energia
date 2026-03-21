bad5c6c2a02294aa918e1785df87fbe847f419c7
Bianca Santos <bianca.santtos6@hotmail.com>
2026-03-21 18:16:22 -0300

    Entrega do projeto.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculadora de Energia
+Este programa calcula quanto um eletrodoméstico gasta por mês.
+
+- **Linguagem:** Python
+- **Fórmula:** (Potência * Horas * 30) / 1000
+
+
---
+++ app.py
@@ -0,0 +1,13 @@
+# Perguntas para o usuário
+aparelho = input("Qual o nome do aparelho? ")
+potencia = float(input("Qual a potência em Watts (W)? "))
+horas_dia = float(input("Quantas horas ele é usado por dia? "))
+
+# Conta matemática
+consumo_mensal = (potencia * horas_dia * 30) / 1000
+
+# Mostra o resultado na tela
+print("-" * 30)
+print(f"Aparelho: {aparelho}")
+print(f"Consumo mensal: {consumo_mensal} kWh/mês")
+print("-" * 30)
